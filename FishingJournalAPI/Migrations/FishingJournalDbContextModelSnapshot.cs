3466d39f0b298a9beb1616844d8ff1e15c85dbe6
Piggo41 <mr.piggo123@gmail.com>
2023-06-28 21:25:00 +0200

    added Migrations folder to .gitignore

--- FishingJournalAPI/Migrations/FishingJournalDbContextModelSnapshot.cs
+++ FishingJournalAPI/Migrations/FishingJournalDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace FishingJournal.API.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.8");
 
-            modelBuilder.Entity("FishingJournal.API.Models.FishType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.FishType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -31,7 +31,7 @@ namespace FishingJournal.API.Migrations
                     b.ToTable("FishTypes");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.HookType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.HookType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -45,7 +45,7 @@ namespace FishingJournal.API.Migrations
                     b.ToTable("HookTypes");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.JournalEntry", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.JournalEntry", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -96,11 +96,15 @@ namespace FishingJournal.API.Migrations
 
                     b.Property<int>("FishTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "fishType");
+                        .HasAnnotation("Relational:JsonPropertyName", "fishTypeId");
 
                     b.Property<int>("HookTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "hookType");
+                        .HasAnnotation("Relational:JsonPropertyName", "hookTypeId");
+
+                    b.Property<float>("Latitude")
+                        .HasColumnType("REAL")
+                        .HasAnnotation("Relational:JsonPropertyName", "latitude");
 
                     b.Property<float>("Length")
                         .HasColumnType("REAL")
@@ -110,17 +114,25 @@ namespace FishingJournal.API.Migrations
                         .HasColumnType("TEXT")
                         .HasAnnotation("Relational:JsonPropertyName", "locationName");
 
+                    b.Property<float>("Longitude")
+                        .HasColumnType("REAL")
+                        .HasAnnotation("Relational:JsonPropertyName", "longitude");
+
                     b.Property<int>("RigTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "rigType");
+                        .HasAnnotation("Relational:JsonPropertyName", "rigTypeId");
+
+                    b.Property<int>("UserId")
+                        .HasColumnType("INTEGER")
+                        .HasAnnotation("Relational:JsonPropertyName", "userId");
 
                     b.Property<int>("WaterCurrentTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "waterCurrentType");
+                        .HasAnnotation("Relational:JsonPropertyName", "waterCurrentTypeId");
 
                     b.Property<int>("WaterSurfaceTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "waterSurfaceType");
+                        .HasAnnotation("Relational:JsonPropertyName", "waterSurfaceTypeId");
 
                     b.Property<float>("WaterTemperature")
                         .HasColumnType("REAL")
@@ -128,7 +140,7 @@ namespace FishingJournal.API.Migrations
 
                     b.Property<int>("WeatherTypeId")
                         .HasColumnType("INTEGER")
-                        .HasAnnotation("Relational:JsonPropertyName", "weatherType");
+                        .HasAnnotation("Relational:JsonPropertyName", "weatherTypeId");
 
                     b.Property<float>("Weight")
                         .HasColumnType("REAL")
@@ -144,10 +156,24 @@ namespace FishingJournal.API.Migrations
 
                     b.HasKey("Id");
 
+                    b.HasIndex("FishTypeId");
+
+                    b.HasIndex("HookTypeId");
+
+                    b.HasIndex("RigTypeId");
+
+                    b.HasIndex("UserId");
+
+                    b.HasIndex("WaterCurrentTypeId");
+
+                    b.HasIndex("WaterSurfaceTypeId");
+
+                    b.HasIndex("WeatherTypeId");
+
                     b.ToTable("JournalEntries");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.RigType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.RigType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -161,35 +187,7 @@ namespace FishingJournal.API.Migrations
                     b.ToTable("RigTypes");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.User", b =>
-                {
-                    b.Property<int>("Id")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<string>("Name")
-                        .HasMaxLength(32)
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
-                    b.Property<byte[]>("Salt")
-                        .IsRequired()
-                        .HasColumnType("BLOB");
-
-                    b.HasKey("Id", "Name");
-
-                    b.HasIndex("Id")
-                        .IsUnique();
-
-                    b.HasIndex("Name")
-                        .IsUnique();
-
-                    b.ToTable("Users");
-                });
-
-            modelBuilder.Entity("FishingJournal.API.Models.WaterCurrentType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WaterCurrentType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -203,7 +201,7 @@ namespace FishingJournal.API.Migrations
                     b.ToTable("WaterCurrentTypes");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.WaterSurfaceType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WaterSurfaceType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -217,7 +215,7 @@ namespace FishingJournal.API.Migrations
                     b.ToTable("WaterSurfaceTypes");
                 });
 
-            modelBuilder.Entity("FishingJournal.API.Models.WeatherType", b =>
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WeatherType", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -229,6 +227,129 @@ namespace FishingJournal.API.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("WeatherTypes");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(32)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Role")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<byte[]>("Salt")
+                        .IsRequired()
+                        .HasColumnType("BLOB");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Id")
+                        .IsUnique();
+
+                    b.HasIndex("Name")
+                        .IsUnique();
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.JournalEntry", b =>
+                {
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.FishType", "FishType")
+                        .WithMany("Parents")
+                        .HasForeignKey("FishTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.HookType", "HookType")
+                        .WithMany("Parents")
+                        .HasForeignKey("HookTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.RigType", "RigType")
+                        .WithMany("Parents")
+                        .HasForeignKey("RigTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.User", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.WaterCurrentType", "WaterCurrentType")
+                        .WithMany("Parents")
+                        .HasForeignKey("WaterCurrentTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.WaterSurfaceType", "WaterSurfaceType")
+                        .WithMany("Parents")
+                        .HasForeignKey("WaterSurfaceTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("FishingJournal.API.Models.JournalEntryModels.WeatherType", "WeatherType")
+                        .WithMany("Parents")
+                        .HasForeignKey("WeatherTypeId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("FishType");
+
+                    b.Navigation("HookType");
+
+                    b.Navigation("RigType");
+
+                    b.Navigation("User");
+
+                    b.Navigation("WaterCurrentType");
+
+                    b.Navigation("WaterSurfaceType");
+
+                    b.Navigation("WeatherType");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.FishType", b =>
+                {
+                    b.Navigation("Parents");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.HookType", b =>
+                {
+                    b.Navigation("Parents");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.RigType", b =>
+                {
+                    b.Navigation("Parents");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WaterCurrentType", b =>
+                {
+                    b.Navigation("Parents");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WaterSurfaceType", b =>
+                {
+                    b.Navigation("Parents");
+                });
+
+            modelBuilder.Entity("FishingJournal.API.Models.JournalEntryModels.WeatherType", b =>
+                {
+                    b.Navigation("Parents");
                 });
 #pragma warning restore 612, 618
         }
