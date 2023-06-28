02fc9db779225dc15f0278478c3e2c2d7d487e18
Piggo41 <mr.piggo123@gmail.com>
2023-06-28 21:28:02 +0200

    added MappingProfile.cs

---
+++ FishingJournalAPI/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FishingJournal.API.Models;
+using FishingJournal.API.Models.InputModels;
+using FishingJournal.API.Models.JournalEntryModels;
+
+namespace FishingJournal.API.Helpers
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile() 
+        {
+            CreateMap<RegisterInputModel, User>().ReverseMap();
+        }
+    }
+}
