0a346faa53a85d0a4e539125c76bf3efe8d4324f
Piggo41 <mr.piggo123@gmail.com>
2023-06-29 01:05:46 +0200

    Removed Salt property

--- FishingJournalAPI/Models/User.cs
+++ FishingJournalAPI/Models/User.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 namespace FishingJournal.API.Models
@@ -13,22 +11,19 @@ namespace FishingJournal.API.Models
         [Key]
         [Required]
         public string Id { get; set; }
+
         /// <summary>
         /// The username
         /// </summary>
         [Required]
         [StringLength(32)]
         public string Name { get; set; }
+
         /// <summary>
-        /// User's hashed password
+        /// User's hashed and salted password
         /// </summary>
         [Required]
         public string Password { get; set; }
-        /// <summary>
-        /// Salt of the hashed password
-        /// </summary>
-        [Required]
-        public byte[] Salt { get; set; }
 
         /// <summary>
         /// The Role of the user
