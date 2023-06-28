9335e4a7ff3c278a1388f76d1a708912731d04e5
Piggo41 <mr.piggo123@gmail.com>
2023-06-29 01:05:57 +0200

    Added InputModels for Controllers

---
+++ FishingJournalAPI/Models/InputModels/EntryModificationInputModel.cs
@@ -0,0 +1,16 @@
+﻿using FishingJournal.API.Models.JournalEntryModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace FishingJournal.API.Models.InputModels
+{
+    public class EntryModificationInputModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public int JournalEntryId { get; set; }
+
+        public JournalEntry? NewJournalEntry { get; set; }
+    }
+}
