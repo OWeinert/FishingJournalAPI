f5c14a8a6738c503971f8e0ce0b27a03fdaf11c0
Piggo41 <mr.piggo123@gmail.com>
2023-06-30 00:24:16 +0200

    Added nullable warning disable to EntryModificationInputModel

--- FishingJournalAPI/Models/InputModels/EntryModificationInputModel.cs
+++ FishingJournalAPI/Models/InputModels/EntryModificationInputModel.cs
@@ -1,6 +1,7 @@
 ﻿using FishingJournal.API.Models.JournalEntryModels;
 using System.ComponentModel.DataAnnotations;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 namespace FishingJournal.API.Models.InputModels
 {
     public class EntryModificationInputModel
